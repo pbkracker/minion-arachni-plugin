e5bf2f9399c9d46d14949d95af3e1beeddffbd55
pbkracker <patrick.kelley@hackerdojo.com>
2013-04-23 16:35:01 -0700

    Making scan parameters dynamic.
    Scan parameters are now populated from command line arguments.  Sane defaults are used when a parameter is not provided.

--- minion/plugins/arachni_runner.rb
+++ minion/plugins/arachni_runner.rb
@@ -3,16 +3,58 @@
 require 'arachni/rpc/pure'
 require 'cgi'
 require 'pp'
+require 'optparse'
+
+options = {}
+opt_parser = OptionParser.new do |opt|
+  opt.banner = "Usage: arachni_runner.rb [options]"
+  # Some sane defaults.
+  options[:url] = 'http://testfire.net'
+  options[:links] = true
+  options[:linkcount] = 5
+  options[:forms] = true
+  options[:audit_cookies] = false
+  options[:headers] = false
+  options[:modules] = 'xss*'
+  options[:subdomains] = false
+
+  opt.on("-url", "--url URL", "URL to scan") do |url|
+    options[:url] = url
+  end
+  opt.on("-links", "Audit Links?") do
+    options[:links] = true
+  end
+  opt.on("-lc", "--link_count 5", "Links to follow") do |link_count|
+    options[:linkcount] = link_count
+  end
+  opt.on("-forms", "Audit Forms?") do
+    options[:forms] = true
+  end
+  opt.on("-cookies", "Audit Cookies?") do
+    options[:audit_cookies] = true
+  end
+  opt.on("-headers", "Audit Headers?") do
+    options[:headers] = true
+  end
+  opt.on("-mods", "--modules MODS", "Arachni Modules to use") do |mods|
+    options[:modules] = mods
+  end
+  opt.on("-fsd", "--follow-sub-domains", "Follow Sub-Domains?") do
+    options[:subdomains] = true
+  end
+
+  opt.on('-h', '--help', "Display this screen" ) do
+    puts opt
+    exit
+  end
+end
+
+opt_parser.parse!
+
 
 # You must have arachni_rpcd running for this to work.
 # This currently relies on the experimental branch of arachni.
 # https://github.com/Arachni/arachni/tree/experimental#source
-
-url_to_scan = 'http://testfire.net'
-if ARGV.size > 0
-        url_to_scan = ARGV[0]
-end
-puts "Arachni will scan this URL: " + url_to_scan
 
 
 # TODO: Read cookies from a file or from the argument list.
@@ -46,21 +88,20 @@ service = Arachni::RPC::RemoteObjectMapper.new( instance, 'service' )
 
 #puts static_cookies
 
-service.scan url: url_to_scan,
-             audit_links: true,
-             audit_forms: true,
-             link_count_limit: 5,
+service.scan url: options[:url],
+             audit_links: options[:links],
+             link_count_limit: options[:linkcount],
+             audit_forms: options[:forms],
+             audit_cookies: options[:audit_cookies],
+             audit_headers: options[:headers],
+             modules: options[:modules],
+             follow_subdomains: options[:subdomains],
              cookies: static_cookies,
-             audit_cookies: true,
-             audit_headers: true,
              exclude: ['SignOut'],
-             debug: false,
-             # load all XSS modules
-             modules: 'xss*'
-             # Just audit the first page.
              #proxy_host: '10.2.178.166',
              #proxy_port: '8080',
-             #follow_subdomains: true
+             debug: false
+             
 
 
 while sleep 1
